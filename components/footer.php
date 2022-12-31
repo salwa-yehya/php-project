95d446af24d6e1096e497ec64693108eab574c50
BayanALriyati <115423659+BayanALriyati@users.noreply.github.com>
2022-12-31 22:03:48 +0300

    secation category

--- components/footer.php
+++ components/footer.php
@@ -4,12 +4,12 @@
 
       <div class="box">
          <h3>quick links</h3>
-         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
-         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
-         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
-         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+         <a href="home.php"> <i class="fas fa-home"></i> home</a>
+         <a href="about.php"> <i class="fas fa-question"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-shop"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-address-card"></i> contact</a>
       </div>
-
+     
       <div class="box">
          <h3>extra links</h3>
          <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
